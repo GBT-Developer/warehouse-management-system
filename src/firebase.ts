172597c1e98bf78893930a6c94e8ffb5edc43948
Farouq Abdurrahman <farouq03@gmail.com>
2023-08-09 19:12:38 +0200

    firebase error

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,16 +1,23 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const firebaseConfig = {
-    apiKey: process.env.VITE_APP_FIREBASE_API_KEY as string,
-    authDomain: process.env.VITE_APP_FIREBASE_AUTH_DOMAIN as string,
-    projectId: process.env.VITE_APP_FIREBASE_PROJECT_ID as string,
-    storageBucket: process.env.VITE_APP_FIREBASE_STORAGE_BUCKET as string,
-    messagingSenderId: process.env
-        .VITE_APP_FIREBASE_MESSAGING_SENDER_ID as string,
-    appId: process.env.VITE_APP_FIREBASE_APP_ID as string,
-    measurementId: process.env.VITE_APP_FIREBASE_MEASUREMENT_ID as string,
+  apiKey: process.env.VITE_APP_FIREBASE_API_KEY as string,
+  authDomain: process.env.VITE_APP_FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.VITE_APP_FIREBASE_PROJECT_ID as string,
+  storageBucket: process.env.VITE_APP_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: process.env
+    .VITE_APP_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.VITE_APP_FIREBASE_APP_ID as string,
+  measurementId: process.env.VITE_APP_FIREBASE_MEASUREMENT_ID as string,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,10 +1,21 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { auth } from 'firebase';
+import { signInAnonymously } from 'firebase/auth';
 
 function Hello() {
   return (
     <div>
       <h1 className="text-blue-400 text-5xl">Hello World!</h1>
+      <button
+        type="button"
+        className="bg-gray-300 text-black p-2 rounded-lg shadow-md hover:bg-gray-400"
+        onClick={() => {
+          signInAnonymously(auth);
+        }}
+      >
+        Click me
+      </button>
     </div>
   );
 }
