1fbff63ae198b8eaff3653589812154e5c38f99c
Farouq Abdurrahman <farouq03@gmail.com>
2023-08-12 23:32:24 +0200

    Update input field styling

---
+++ src/renderer/components/StockInputField.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+interface StockInputFieldProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  labelFor: string;
+  label: string;
+}
+
+export const StockInputField = ({
+  value,
+  onChange,
+  labelFor,
+  label,
+}: StockInputFieldProps) => {
+  return (
+    <div>
+      <label
+        htmlFor={labelFor}
+        className="text-sm font-medium text-gray-900 dark:text-white flex flex-col gap-1"
+      >
+        {label}
+        <input
+          id={labelFor}
+          name={labelFor}
+          type="text"
+          className="placeholder:text-xs placeholder:font-light bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Masukan teks.."
+          value={value}
+          onChange={onChange}
+        />
+      </label>
+    </div>
+  );
+};
--- src/renderer/pages/InputPage.tsx
+++ src/renderer/pages/InputPage.tsx
@@ -1,9 +1,10 @@
 import { addDoc, collection } from 'firebase/firestore';
 import { db } from 'firebase';
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import { AppLayout } from 'renderer/layout/AppLayout';
 import { Product } from 'renderer/interfaces/Product';
+import { StockInputField } from 'renderer/components/StockInputField';
+import { useNavigate } from 'react-router-dom';
 
 const newProductInitialState = {
   brand: '',
@@ -18,9 +19,36 @@ const newProductInitialState = {
 export const InputPage = () => {
   const [newProduct, setNewProduct] = useState<Product>(newProductInitialState);
   const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   async function handleSubmit(e: { preventDefault: () => void }) {
     e.preventDefault();
+
+    // If one or more fields are empty, return early
+    if (
+      Object.values(newProduct).some(
+        (value) => value === '' || value === undefined
+      )
+    ) {
+      setErrorMessage('Mohon isi semua kolom');
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 3000);
+      return;
+    }
+
+    if (
+      Number.isNaN(Number(newProduct.price)) ||
+      Number.isNaN(Number(newProduct.count)) ||
+      Number(newProduct.price) <= 0 ||
+      Number(newProduct.count) <= 0
+    ) {
+      setErrorMessage('Harga atau jumlah barang tidak valid');
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 3000);
+      return;
+    }
 
     const productCollection = collection(db, '/product');
 
@@ -32,79 +60,92 @@ export const InputPage = () => {
 
   return (
     <AppLayout>
-      <button
-        type="button"
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
-        Back
-      </button>
-      <form onSubmit={handleSubmit}>
-        <input
-          className="text-black"
-          type="text"
-          placeholder="Merek"
-          value={newProduct.brand}
-          onChange={(e) =>
-            setNewProduct({ ...newProduct, brand: e.target.value })
-          }
-        />
-        <input
-          className="text-black"
-          type="text"
-          placeholder="Jenis motor"
-          value={newProduct.motor_type}
-          onChange={(e) =>
-            setNewProduct({ ...newProduct, motor_type: e.target.value })
-          }
-        />
-        <input
-          className="text-black"
-          type="text"
-          placeholder="Part"
-          value={newProduct.part}
-          onChange={(e) =>
-            setNewProduct({ ...newProduct, part: e.target.value })
-          }
-        />
-        <input
-          className="text-black"
-          type="text"
-          placeholder="Warna tersedia"
-          value={newProduct.available_color}
-          onChange={(e) =>
-            setNewProduct({ ...newProduct, available_color: e.target.value })
-          }
-        />
-        <input
-          className="text-black"
-          type="text"
-          placeholder="Jumlah barang"
-          value={newProduct.count}
-          onChange={(e) =>
-            setNewProduct({ ...newProduct, count: e.target.value })
-          }
-        />
-        <input
-          className="text-black"
-          type="text"
-          placeholder="Harga barang"
-          value={newProduct.price}
-          onChange={(e) =>
-            setNewProduct({ ...newProduct, price: e.target.value })
-          }
-        />
-        <input
-          className="text-black"
-          type="text"
-          placeholder="Posisi gudang"
+      <h1 className="mb-4 text-4xl font-extrabold tracking-tight text-gray-900 md:text-5xl dark:text-white">
+        Input Stock
+      </h1>
+      <form onSubmit={handleSubmit} className="w-full flex flex-col gap-3">
+        <div className="grid gap-3 md:grid-cols-2 w-full">
+          <StockInputField
+            labelFor="brand"
+            label="Merek"
+            value={newProduct.brand}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, brand: e.target.value })
+            }
+          />
+          <StockInputField
+            labelFor="motor_type"
+            label="Jenis motor"
+            value={newProduct.motor_type}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, motor_type: e.target.value })
+            }
+          />
+          <StockInputField
+            labelFor="part"
+            label="Part"
+            value={newProduct.part}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, part: e.target.value })
+            }
+          />
+          <StockInputField
+            labelFor="available_color"
+            label="Warna tersedia"
+            value={newProduct.available_color}
+            onChange={(e) =>
+              setNewProduct({
+                ...newProduct,
+                available_color: e.target.value,
+              })
+            }
+          />
+          <StockInputField
+            labelFor="count"
+            label="Jumlah barang"
+            value={newProduct.count}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, count: e.target.value })
+            }
+          />
+          <StockInputField
+            labelFor="price"
+            label="Harga barang (Rp)"
+            value={newProduct.price}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, price: e.target.value })
+            }
+          />
+        </div>
+        <StockInputField
+          labelFor="warehouse_position"
+          label="Posisi gudang"
           value={newProduct.warehouse_position}
           onChange={(e) =>
-            setNewProduct({ ...newProduct, warehouse_position: e.target.value })
+            setNewProduct({
+              ...newProduct,
+              warehouse_position: e.target.value,
+            })
           }
         />
-        <button type="submit">Input</button>
+        {errorMessage && (
+          <p className="text-red-500 text-sm ">{errorMessage}</p>
+        )}
+        <div className="flex flex-row-reverse gap-2 w-full justify-start">
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+          <button
+            type="button"
+            className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            onClick={() => navigate(-1)}
+          >
+            Cancel
+          </button>
+        </div>
       </form>
     </AppLayout>
   );
