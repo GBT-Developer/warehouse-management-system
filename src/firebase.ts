dccb5557118436d35e874f5706195ad4b1c5c004
Farouq Abdurrahman <farouq03@gmail.com>
2023-08-18 23:40:06 +0200

    implement using emulator

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-import { getFunctions } from 'firebase/functions';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import {
+  getFunctions,
+  httpsCallable,
+  connectFunctionsEmulator,
+} from 'firebase/functions';
 
 const firebaseConfig = {
   apiKey: window.process.env.FIREBASE_API_KEY,
@@ -18,7 +22,17 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const functions = getFunctions(app);
+export const functions = getFunctions(app, 'asia-southeast2');
+export const callable = (name: string) => httpsCallable(functions, name);
+
+if (window.process.env.NODE_ENV === 'development') {
+  console.log('Connecting to local emulators');
+  connectFirestoreEmulator(db, 'localhost', 8080);
+  connectAuthEmulator(auth, 'http://localhost:9099');
+  connectFunctionsEmulator(functions, 'localhost', 5001);
+} else {
+  console.log('Connecting to production');
+}
 
 export function getFirebaseConfig() {
   if (!firebaseConfig || !firebaseConfig.apiKey) {
