1416c6cbf41cf0039f9454d4bd0b4037fb601143
Farouq Abdurrahman <farouq03@gmail.com>
2023-08-20 21:12:01 +0200

    use table layout

--- warehouse-management-system/src/renderer/pages/ManageStockPage.tsx
+++ warehouse-management-system/src/renderer/pages/ManageStockPage.tsx
@@ -2,9 +2,12 @@ import { db } from 'firebase';
 import { collection, doc, getDocs, query, updateDoc } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import { AiFillEdit } from 'react-icons/ai';
-import { BsChevronLeft, BsChevronRight, BsSearch } from 'react-icons/bs';
+import { SingleTableItem } from 'renderer/components/TableComponents/SingleTableItem';
+import { TableHeader } from 'renderer/components/TableComponents/TableHeader';
+import { TableTitle } from 'renderer/components/TableComponents/TableTitle';
 import { Product } from 'renderer/interfaces/Product';
 import { PageLayout } from 'renderer/layout/PageLayout';
+import { useAuth } from 'renderer/providers/AuthProvider';
 
 export const ManageStockPage = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -12,6 +15,9 @@ export const ManageStockPage = () => {
   const [loading, setLoading] = useState(true);
   const [editingIndex, setEditingIndex] = useState(null);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [search, setSearch] = useState('');
+  const { user } = useAuth();
+
   // take product from firebase
   useEffect(() => {
     const fetchData = async () => {
@@ -80,53 +86,29 @@ export const ManageStockPage = () => {
 
   return (
     <PageLayout>
-      <section className="bg-gray-50 w-full h-full px-3 bg-transparent overflow-hidden">
+      <div className="w-full h-full bg-transparent overflow-hidden">
         <div className="relative shadow-md sm:rounded-lg overflow-auto h-full flex flex-col justify-between">
-          <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
-            <div className="w-full md:w-1/2 flex items-center">
-              <div className="relative w-full">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <BsSearch />
-                </div>
-                <input
-                  type="text"
-                  id="simple-search"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-                  placeholder="Cari nama produk"
-                  required
-                />
-              </div>
-            </div>
-          </div>
+          <TableTitle setSearch={setSearch} />
           <div className="overflow-y-auto h-full">
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-              <thead className="sticky top-0 text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                  <th scope="col" className="px-4 py-3">
-                    Nama Produk
-                  </th>
-                  <th
-                    scope="col"
-                    className="py-3 pl-4 pr-[4rem] flex justify-end"
-                  >
-                    Jumlah
-                  </th>
-                </tr>
-              </thead>
+              <TableHeader>
+                <th className="px-4 py-3">No</th>
+                <th className="px-4 py-3">Merk</th>
+                <th className="px-4 py-3">Part</th>
+                <td className="px-4 py-3">Jumlah</td>
+              </TableHeader>
               <tbody className="overflow-y-auto">
                 {products.map((product: Product, index) => (
                   <tr key={index} className="border-b dark:border-gray-700">
-                    <th
-                      scope="row"
-                      className="px-4 py-3 flex-1 font-medium text-gray-900 dark:text-white max-w-xs"
-                    >
-                      {`${product.part} ${product.brand}`}
-                    </th>
-                    <td className="px-4 py-3 flex-1 max-w-xs">
-                      <div className="flex items-center gap-6 justify-end">
+                    <SingleTableItem>{index + 1}</SingleTableItem>
+                    <SingleTableItem>{`${product.part}`}</SingleTableItem>
+                    <SingleTableItem>{`${product.part}`}</SingleTableItem>
+
+                    <SingleTableItem>
+                      <div className="flex justify-start gap-[1rem]">
                         <input
                           type="text"
-                          className="w-20 text-center text-gray-900 dark:text-white bg-gray-100 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md focus:border-primary-500 focus:ring-primary-500"
+                          className="w-16 text-center text-gray-900 dark:text-white border border-gray-300 dark:border-gray-600 rounded-md focus:border-primary-500 focus:ring-primary-500"
                           value={total[index]}
                           onChange={(e) =>
                             handleTotalChange(index, e.target.value)
@@ -142,67 +124,14 @@ export const ManageStockPage = () => {
                           <AiFillEdit />
                         </button>
                       </div>
-                    </td>
+                    </SingleTableItem>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
-          <nav className="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4">
-            <div className="flex gap-2 text-sm font-normal text-gray-500 dark:text-gray-400">
-              Showing
-              <p className="font-semibold text-gray-900 dark:text-white">1-1</p>
-              of
-              <p className="font-semibold text-gray-900 dark:text-white">1</p>
-            </div>
-            <ul className="inline-flex items-stretch -space-x-px">
-              <li>
-                <a
-                  href="/"
-                  className="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  <BsChevronLeft />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  1
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  2
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/"
-                  className="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                >
-                  <BsChevronRight />
-                </a>
-              </li>
-            </ul>
-            {errorMessage && (
-              <p className="text-red-500 text-sm ">{errorMessage}</p>
-            )}
-          </nav>
-          <button
-            disabled={loading}
-            type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            onClick={handleSubmit}
-          >
-            Update
-          </button>
         </div>
-      </section>
+      </div>
     </PageLayout>
   );
 };
