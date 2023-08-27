6de41cef7bc4061f38d86bae149db77694bbb40b
Farouq Abdurrahman <farouq03@gmail.com>
2023-08-27 09:53:30 +0200

    implement trigger function for stock history

--- cloud-functions/functions/src/updateProductStock/index.ts
+++ cloud-functions/functions/src/updateProductStock/index.ts
@@ -2,20 +2,25 @@ import { onDocumentWritten } from "firebase-functions/v2/firestore";
 import { firebaseAdmin } from "..";
 
 exports.updateProductStock = onDocumentWritten("product/{docId}", (event) => {
-  console.log(`Document products is updated`, event);
   if (!event.data) {
-    console.log(`Document ${event.params.docId} updated at ${event.data}`);
     return;
   }
   const newObject = event.data.after.data() as any;
   const previousObject = event.data.before.data() as any;
-  const difference = newObject.count - previousObject.count;
-  console.log(`Difference is ${difference}`);
 
-  firebaseAdmin.firestore().collection("stock_history").add({
-    product_id: event.params.docId,
-    product_name: newObject.brand,
-    count: difference,
-    created_at: new Date(),
-  });
+  const productRef = firebaseAdmin
+    .firestore()
+    .collection("product")
+    .doc(event.params.docId);
+
+  firebaseAdmin
+    .firestore()
+    .collection("stock_history")
+    .add({
+      product: productRef,
+      old_count: previousObject.count,
+      new_count: newObject.count,
+      count: newObject.count - previousObject.count,
+      updated_at: new Date(),
+    });
 });
