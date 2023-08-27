025dea0c09d3d5b26db22365c537447883bf9355
Farouq Abdurrahman <farouq03@gmail.com>
2023-08-27 10:56:38 +0200

    finish new product page design

--- wms-frontend/src/renderer/pages/NewProductPage.tsx
+++ wms-frontend/src/renderer/pages/NewProductPage.tsx
@@ -18,9 +18,11 @@ const newProductInitialState = {
   warehouse_position: '',
   count: '',
   supplier: '',
+  sell_price: '',
+  buy_price: '',
 };
 
-export const ManageProductPage = () => {
+export const NewProductPage = () => {
   const [newProduct, setNewProduct] = useState<Product>(newProductInitialState);
   const navigate = useNavigate();
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
@@ -71,10 +73,12 @@ export const ManageProductPage = () => {
     }
 
     if (
-      Number.isNaN(Number(newProduct.price)) ||
+      Number.isNaN(Number(newProduct.sell_price)) ||
+      Number.isNaN(Number(newProduct.buy_price)) ||
       Number.isNaN(Number(newProduct.count)) ||
       Number.isNaN(Number(newProduct.initial_cost)) ||
-      Number(newProduct.price) <= 0 ||
+      Number(newProduct.sell_price) <= 0 ||
+      Number(newProduct.buy_price) <= 0 ||
       Number(newProduct.count) <= 0
     ) {
       setErrorMessage('Harga atau jumlah barang tidak valid');
@@ -105,142 +109,146 @@ export const ManageProductPage = () => {
 
   return (
     <PageLayout>
-      <h1 className="mb-4 text-4xl font-extrabold tracking-tight text-gray-900 md:text-5xl">
-        Kelola Produk
+      <h1 className="mb-[4rem] text-4xl font-extrabold tracking-tight text-gray-900 md:text-5xl">
+        Add new product
       </h1>
       <form
         onSubmit={handleSubmit}
         className={`w-full flex flex-col gap-3 relative ${
           loading ? 'p-2' : ''
-        }`}
+        } transform transition-all duration-300`}
       >
         {loading && (
           <div className="absolute flex justify-center items-center py-2 px-3 top-0 left-0 w-full h-full bg-gray-50 rounded-lg z-0">
             <AiOutlineLoading3Quarters className="animate-spin flex justify-center text-4xl" />
           </div>
         )}
-        <div className="grid gap-3 md:grid-cols-2 w-full">
-          <StockInputField
-            loading={loading}
-            labelFor="brand"
-            label="Merek"
-            value={newProduct.brand}
-            onChange={(e) =>
-              setNewProduct({ ...newProduct, brand: e.target.value })
-            }
-          />
-          <StockInputField
-            loading={loading}
-            labelFor="motor_type"
-            label="Jenis motor"
-            value={newProduct.motor_type}
-            onChange={(e) =>
-              setNewProduct({ ...newProduct, motor_type: e.target.value })
-            }
-          />
-          <StockInputField
-            loading={loading}
-            labelFor="part"
-            label="Part"
-            value={newProduct.part}
-            onChange={(e) =>
-              setNewProduct({ ...newProduct, part: e.target.value })
-            }
-          />
-          <StockInputField
-            loading={loading}
-            labelFor="available_color"
-            label="Warna tersedia"
-            value={newProduct.available_color.toLowerCase()}
-            onChange={(e) =>
-              setNewProduct({
-                ...newProduct,
-                available_color: e.target.value,
-              })
-            }
-          />
-          <StockInputField
-            loading={loading}
-            labelFor="count"
-            label="Jumlah barang"
-            value={newProduct.count}
-            onChange={(e) =>
-              setNewProduct({ ...newProduct, count: e.target.value })
-            }
-          />
-          <StockInputField
-            loading={loading}
-            labelFor="price"
-            label="Harga Jual (Rp)"
-            value={newProduct.price}
-            onChange={(e) =>
-              setNewProduct({ ...newProduct, price: e.target.value })
-            }
-          />
-          <StockInputField
-            loading={loading}
-            labelFor="price"
-            label="Harga modal (Rp)"
-            value={newProduct.initial_cost}
-            onChange={(e) =>
-              setNewProduct({ ...newProduct, initial_cost: e.target.value })
-            }
-          />
+        <StockInputField
+          loading={loading}
+          labelFor="brand"
+          label="Brand"
+          value={newProduct.brand}
+          onChange={(e) =>
+            setNewProduct({ ...newProduct, brand: e.target.value })
+          }
+        />
+        <StockInputField
+          loading={loading}
+          labelFor="type"
+          label="Motorcycle Type"
+          value={newProduct.motor_type}
+          onChange={(e) =>
+            setNewProduct({ ...newProduct, motor_type: e.target.value })
+          }
+        />
+        <StockInputField
+          loading={loading}
+          labelFor="part"
+          label="Part"
+          value={newProduct.part}
+          onChange={(e) =>
+            setNewProduct({ ...newProduct, part: e.target.value })
+          }
+        />
+        <StockInputField
+          loading={loading}
+          labelFor="available_color"
+          label="Available Color"
+          value={newProduct.available_color}
+          onChange={(e) =>
+            setNewProduct({ ...newProduct, available_color: e.target.value })
+          }
+        />
+        <StockInputField
+          loading={loading}
+          labelFor="count"
+          label="Product Count"
+          value={newProduct.count}
+          onChange={(e) =>
+            setNewProduct({ ...newProduct, count: e.target.value })
+          }
+        />
+        <StockInputField
+          loading={loading}
+          labelFor="pruchase_price"
+          label="Purchase Price"
+          value={newProduct.buy_price}
+          onChange={(e) =>
+            setNewProduct({ ...newProduct, buy_price: e.target.value })
+          }
+        />
+        <StockInputField
+          loading={loading}
+          labelFor="pruchase_price"
+          label="Sell Price"
+          value={newProduct.sell_price}
+          onChange={(e) =>
+            setNewProduct({ ...newProduct, sell_price: e.target.value })
+          }
+        />
+        <div>
+          <div className="flex justify-between">
+            <div className="w-1/3">
+              <label htmlFor={'warehouse'} className="text-md">
+                Warehouse Position
+              </label>
+            </div>
+            <div className="w-2/3">
+              <select
+                ref={selectRef}
+                disabled={loading}
+                id="warehouse-position"
+                name="warehouse-position"
+                onChange={(e) => {
+                  setNewProduct({
+                    ...newProduct,
+                    warehouse_position: e.target.value,
+                  });
+                }}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              >
+                <option value="" selected>
+                  Choose Warehouse
+                </option>
+                <option value="gudang_jadi">Gudang Jadi</option>
+                <option value="gudang_bahan">Gudang Bahan</option>
+              </select>{' '}
+            </div>
+          </div>
         </div>
-        <label
-          htmlFor="countries"
-          className="block mb-2 text-sm font-medium text-gray-900"
-        >
-          Posisi barang di gudang
-          <select
-            ref={selectRef}
-            disabled={loading}
-            id="countries"
-            name="countries"
-            onChange={(e) => {
-              setNewProduct({
-                ...newProduct,
-                warehouse_position: e.target.value,
-              });
-            }}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-          >
-            <option value="" selected>
-              Pilih gudang
-            </option>
-            <option value="gudang_jadi">Gudang Jadi</option>
-            <option value="gudang_bahan">Gudang Bahan</option>
-          </select>
-        </label>
-        <label
-          htmlFor="countries"
-          className="block mb-2 text-sm font-medium text-gray-900 "
-        >
-          Supplier barang
-          <select
-            ref={selectRef}
-            disabled={loading}
-            id="countries"
-            name="countries"
-            onChange={(e) => {
-              setNewProduct({
-                ...newProduct,
-                supplier: e.target.value,
-              });
-            }}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-          >
-            <option value="" selected>
-              Pilih Supplier
-            </option>
-            {suppliers.map((supplier) => (
-              <option value={supplier.company_name}>
-                {supplier.company_name}
-              </option>
-            ))}
-          </select>
-        </label>
 
+        <div>
+          <div className="flex justify-between">
+            <div className="w-1/3">
+              <label htmlFor={'supplier'} className="text-md">
+                Supplier
+              </label>
+            </div>
+            <div className="w-2/3">
+              <select
+                disabled={loading}
+                id="supplier"
+                name="supplier"
+                onChange={(e) => {
+                  setNewProduct({
+                    ...newProduct,
+                    supplier: e.target.value,
+                  });
+                }}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              >
+                <option value="" selected>
+                  Choose Supplier
+                </option>
+                {suppliers.map((supplier) => (
+                  <option key={supplier.id} value={supplier.id}>
+                    {supplier.company_name}
+                  </option>
+                ))}
+              </select>{' '}
+            </div>
+          </div>
+        </div>
         {errorMessage && (
           <p className="text-red-500 text-sm ">{errorMessage}</p>
         )}
