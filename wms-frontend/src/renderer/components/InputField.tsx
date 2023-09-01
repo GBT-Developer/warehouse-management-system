636c214a6140cb98e92f78976ab37a173e6c62c9
Farouq Abdurrahman <farouq03@gmail.com>
2023-09-01 23:23:12 +0200

    Rework manage stock page

--- wms-frontend/src/renderer/components/InputField.tsx
+++ wms-frontend/src/renderer/components/InputField.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export interface InputFieldProps {
-  value: string;
+  value: string | number;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   labelFor: string;
   label: string;
@@ -20,27 +20,25 @@ export const InputField = ({
   additionalStyle,
 }: InputFieldProps) => {
   return (
-    <div>
-      <div className="flex justify-between items-center">
-        <div className="w-1/3">
-          <label htmlFor={labelFor} className="text-md">
-            {label}
-          </label>
-        </div>
-        <div className="w-2/3">
-          <input
-            disabled={loading}
-            id={labelFor}
-            name={labelFor}
-            type="text"
-            className={`placeholder:text-xs placeholder:font-light bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2 w-full ${
-              additionalStyle ?? ''
-            }`}
-            placeholder={placeholder ? placeholder : ''}
-            value={value}
-            onChange={onChange}
-          />
-        </div>
+    <div className="flex justify-between items-center">
+      <div className="w-1/3">
+        <label htmlFor={labelFor} className="text-md">
+          {label}
+        </label>
+      </div>
+      <div className="w-2/3">
+        <input
+          disabled={loading}
+          id={labelFor}
+          name={labelFor}
+          type="text"
+          className={`placeholder:text-xs placeholder:font-light bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2 w-full ${
+            additionalStyle ?? ''
+          }`}
+          placeholder={placeholder ? placeholder : ''}
+          value={value}
+          onChange={onChange}
+        />
       </div>
     </div>
   );
--- wms-frontend/src/renderer/pages/ManageStockPage.tsx
+++ wms-frontend/src/renderer/pages/ManageStockPage.tsx
@@ -1,177 +1,296 @@
 import { db } from 'firebase';
-import { collection, doc, getDocs, query, updateDoc } from 'firebase/firestore';
-import { useEffect, useRef, useState } from 'react';
-import { AiFillEdit } from 'react-icons/ai';
+import { collection, doc, getDocs, query, where } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { useNavigate } from 'react-router-dom';
-import { SingleTableItem } from 'renderer/components/TableComponents/SingleTableItem';
-import { TableHeader } from 'renderer/components/TableComponents/TableHeader';
-import { TableTitle } from 'renderer/components/TableComponents/TableTitle';
+import { InputField } from 'renderer/components/InputField';
 import { Product } from 'renderer/interfaces/Product';
+import { Supplier } from 'renderer/interfaces/Supplier';
 import { PageLayout } from 'renderer/layout/PageLayout';
-import { useAuth } from 'renderer/providers/AuthProvider';
+
 export const ManageStockPage = () => {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [total, setTotal] = useState<string[]>([]);
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [loading, setLoading] = useState(true);
-  const [editingIndex, setEditingIndex] = useState<number>(-1);
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
-  const [search, setSearch] = useState('');
-  const { user } = useAuth();
-  const [updatedProduct, setUpdatedProduct] = useState('');
   const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [manageStockMode, setManageStockMode] = useState<
+    'purchase' | 'from_other_warehouse' | ''
+  >('');
+  const [supplierList, setSupplierList] = useState<Supplier[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [selectedSupplier, setSelectedSupplier] = useState<Supplier | null>(
+    null
+  );
+  const [selectedWarehouse, setSelectedWarehouse] = useState<
+    'Gudang Jadi' | 'Gudang Bahan' | ''
+  >('');
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+  const [quantity, setQuantity] = useState<number>(0);
+  const [purchasePrice, setPurchasePrice] = useState<number>(0);
+  const [dispatchNote, setDispatchNote] = useState<string>('');
 
-  // Take product from firebase
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const q = query(collection(db, 'product'));
-        const querySnapshot = await getDocs(q);
+    setLoading(true);
 
-        const productData: Product[] = [];
-        querySnapshot.forEach((theProduct) => {
-          const data = theProduct.data() as Product;
-          data.id = theProduct.id;
-          productData.push(data);
+    if (supplierList.length === 0 && manageStockMode === 'purchase') {
+      const fetchSupplierList = async () => {
+        const supplierQuery = query(collection(db, 'supplier'));
+        const res = await getDocs(supplierQuery);
+        const supplierList: Supplier[] = [];
+
+        res.forEach((doc) => {
+          supplierList.push({
+            id: doc.id,
+            ...doc.data(),
+          } as Supplier);
         });
 
-        setProducts(productData);
-        setTotal(productData.map((product) => product.count));
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+        setSupplierList(supplierList);
+      };
 
-    fetchData().catch((error) => {
-      console.error('Error fetching data:', error);
-    });
-  }, []);
+      fetchSupplierList().catch(() => {
+        setErrorMessage('An error occured while fetching supplier data');
+      });
+    }
 
-  const handleTotalChange = (index: number, value: string) => {
-    // Update the total value for the specific row
-    const updatedTotalValues = [...total];
-    updatedTotalValues[index] = value;
-    setTotal(updatedTotalValues);
-  };
+    const fetchProductList = async (
+      supplier_id: string,
+      the_warehouse: string
+    ) => {
+      const productQuery = query(
+        collection(db, 'product'),
+        where('supplier', '==', doc(db, 'supplier', supplier_id)),
+        where('warehouse_position', '==', the_warehouse)
+      );
+      const res = await getDocs(productQuery);
+      const productList: Product[] = [];
 
-  const handleEditClick = (index: number) => {
-    setEditingIndex(index); // Set the editing index to enable editing for this row
-  };
-
-  const handleBlur = () => {
-    setEditingIndex(-1); // Reset the editing index when blurred
-  };
-
-  function handleSubmit(e: { preventDefault: () => void }) {
-    e.preventDefault();
-    // Declare the index
-    let index = 0;
-    // Check for invalid values in the total array
-    total.forEach((value) => {
-      if (Number.isNaN(Number(value))) {
-        setErrorMessage('jumlah barang tidak valid');
-        setTimeout(() => {
-          setErrorMessage(null);
-        }, 3000);
-        return;
-      }
-    });
-    // Update the data in Firebase
-    products.forEach((product) => {
-      if (product.id === updatedProduct)
-        index = products.findIndex((product) => product.id === updatedProduct);
-    });
-    const collectionRef = collection(db, 'product');
-    const docRef = doc(collectionRef, updatedProduct);
-    updateDoc(docRef, { count: total[index] })
-      .then(() => {
-        console.log(`Document ${updatedProduct} successfully updated!`);
-      })
-      .catch((error) => {
-        console.error(`Error updating document ${updatedProduct}:`, error);
+      res.forEach((doc) => {
+        productList.push({
+          id: doc.id,
+          ...doc.data(),
+        } as Product);
       });
 
-    setEditingIndex(-1);
-    inputRef.current?.blur();
-  }
+      setProducts(productList);
+    };
+
+    if (selectedSupplier?.id && selectedWarehouse)
+      fetchProductList(selectedSupplier.id, selectedWarehouse).catch(() => {
+        setErrorMessage('An error occured while fetching product data');
+      });
+
+    setLoading(false);
+  }, [manageStockMode, selectedSupplier, selectedWarehouse]);
+
   return (
     <PageLayout>
-      <div className="w-full h-full bg-transparent overflow-hidden">
-        <div className="relative shadow-md sm:rounded-lg overflow-auto h-full flex flex-col justify-between">
-          <TableTitle setSearch={setSearch} />
-          <div className="overflow-y-auto h-full">
-            <table className="w-full text-sm text-left text-gray-500">
-              <TableHeader>
-                <th className=" py-3">No</th>
-                <th className=" py-3">Merk</th>
-                <th className=" py-3">Part</th>
-                <td className=" py-3">Supplier</td>
-                <td className=" py-3">Warna</td>
-                <td className=" py-3">Posisi</td>
-                <td className=" py-3">Jumlah</td>
-                <td className=" py-3">History</td>
-              </TableHeader>
-              <tbody className="overflow-y-auto">
-                {products.map((product: Product, index) => (
-                  <tr key={index} className="border-b">
-                    <SingleTableItem>{index + 1}</SingleTableItem>
-                    <SingleTableItem>{`${product.brand}`}</SingleTableItem>
-                    <SingleTableItem>{`${product.part}`}</SingleTableItem>
-                    <SingleTableItem>{`${
-                      product.supplier?.company_name ?? '-'
-                    }`}</SingleTableItem>
-                    <SingleTableItem>{`${product.available_color}`}</SingleTableItem>
-                    <SingleTableItem>{`${product.warehouse_position}`}</SingleTableItem>
-                    <SingleTableItem>
-                      <form
-                        className="flex justify-start gap-[1rem]"
-                        onSubmit={(e) => handleSubmit(e)}
-                      >
-                        <input
-                          ref={inputRef}
-                          type="text"
-                          className="w-20 text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-md focus:border-primary-500 focus:ring-primary-500"
-                          value={total[index]}
-                          onChange={(e) =>
-                            handleTotalChange(index, e.target.value)
-                          }
-                          disabled={editingIndex !== index}
-                          onBlur={handleBlur}
-                        />
-                        <button
-                          type="button"
-                          className="text-gray-500 p-2 hover:text-gray-700 cursor-pointer bg-gray-100 rounded-md"
-                          onClick={() => {
-                            handleEditClick(index);
-                            if (product.id) setUpdatedProduct(product.id);
-                          }}
-                        >
-                          <AiFillEdit />
-                        </button>
-                      </form>
-                    </SingleTableItem>
-                    <SingleTableItem>
-                      <button
-                        type="button"
-                        className="text-gray-500 p-2 hover:text-gray-700 cursor-pointer bg-gray-100 rounded-md"
-                        onClick={() => {
-                          navigate('/stockhistory');
-                        }}
-                      >
-                        Riwayat Produk
-                      </button>
-                    </SingleTableItem>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            {errorMessage && (
-              <p className="text-red-500 text-sm ">{errorMessage}</p>
-            )}
+      <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 md:text-5xl">
+        Manage Stock
+      </h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log('submit');
+        }}
+        className={`w-2/3 py-14 my-10 flex flex-col gap-3 relative${
+          loading ? 'p-2' : ''
+        }`}
+      >
+        {loading && (
+          <div className="absolute flex justify-center items-center py-2 px-3 top-0 left-0 w-full h-full bg-gray-50 rounded-lg z-0">
+            <AiOutlineLoading3Quarters className="animate-spin flex justify-center text-4xl" />
+          </div>
+        )}
+        <div className="flex justify-between">
+          <div className="w-1/3 flex items-center">
+            <label htmlFor={'change-of-stock-mode'} className="text-md">
+              Change of stock mode
+            </label>
+          </div>
+          <div className="w-2/3">
+            <select
+              defaultValue={''}
+              disabled={loading}
+              name="change-of-stock-mode"
+              onChange={(e) => {
+                if (
+                  e.target.value === 'purchase' ||
+                  e.target.value === 'from_other_warehouse'
+                )
+                  setManageStockMode(e.target.value);
+              }}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            >
+              <option value={''} disabled>
+                Choose change of stock mode
+              </option>
+              <option value="purchase">Purchase</option>
+              <option value="from_other_warehouse">
+                From raw material warehouse
+              </option>
+            </select>
           </div>
         </div>
-      </div>
+        <div className="flex justify-between">
+          <div className="w-1/3 flex items-center">
+            <label htmlFor={'warehouse-id'} className="text-md">
+              Choose warehouse
+            </label>
+          </div>
+          <div className="w-2/3">
+            <select
+              defaultValue={''}
+              disabled={loading}
+              name="warehouse-id"
+              onChange={(e) => {
+                if (
+                  e.target.value === 'Gudang Jadi' ||
+                  e.target.value === 'Gudang Bahan'
+                )
+                  setSelectedWarehouse(e.target.value);
+              }}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            >
+              <option value={''} disabled>
+                Choose warehouse
+              </option>
+              {manageStockMode === 'purchase' ? (
+                <>
+                  <option value="Gudang Bahan">Gudang Bahan</option>
+                  <option value="Gudang Jadi">Gudang Jadi</option>
+                </>
+              ) : manageStockMode === 'from_other_warehouse' ? (
+                <>
+                  <option value="Gudang Jadi">Gudang Jadi</option>
+                </>
+              ) : null}
+            </select>
+          </div>
+        </div>
+        {
+          manageStockMode === 'purchase' ? (
+            <div className="flex justify-between">
+              <div className="w-1/3 flex items-center">
+                <label htmlFor={'supplier-id'} className="text-md">
+                  Choose supplier
+                </label>
+              </div>
+              <div className="w-2/3">
+                <select
+                  defaultValue={''}
+                  disabled={loading}
+                  name="supplier-id"
+                  onChange={(e) => {
+                    setSelectedSupplier(
+                      () =>
+                        supplierList.find(
+                          (supplier) => supplier.id === e.target.value
+                        ) ?? null
+                    );
+                  }}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                >
+                  <option value={''} disabled>
+                    Choose supplier
+                  </option>
+                  {supplierList.map((supplier) => (
+                    <option key={supplier.id} value={supplier.id}>
+                      {supplier.company_name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+          ) : manageStockMode === 'from_other_warehouse' ? (
+            <InputField
+              loading={loading}
+              label="Dispatch note"
+              labelFor="dispatch-note"
+              value={dispatchNote}
+              onChange={(e) => setDispatchNote(() => e.target.value)}
+            />
+          ) : null // Default
+        }
+        <div className="flex justify-between">
+          <div className="w-1/3 flex items-center">
+            <label htmlFor={'product-id'} className="text-md">
+              Choose product
+            </label>
+          </div>
+          <div className="w-2/3">
+            <select
+              defaultValue={''}
+              disabled={loading}
+              name="product-id"
+              onChange={(e) => {
+                setSelectedProduct(
+                  () =>
+                    products.find((product) => product.id === e.target.value) ??
+                    null
+                );
+              }}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            >
+              <option value={''} disabled>
+                Choose product
+              </option>
+              {products
+                .filter((product) => {
+                  if (manageStockMode === 'purchase')
+                    if (selectedSupplier)
+                      return product.supplier?.id === selectedSupplier.id;
+                })
+                .map((product) => (
+                  <option key={product.id} value={product.id}>
+                    {product.brand +
+                      ' ' +
+                      product.motor_type +
+                      ' ' +
+                      product.part +
+                      ' ' +
+                      product.available_color}
+                  </option>
+                ))}
+            </select>
+          </div>
+        </div>
+        <InputField
+          loading={loading}
+          label="Quantity"
+          labelFor="quantity"
+          value={quantity.toString()}
+          onChange={(e) => setQuantity(() => parseInt(e.target.value))}
+        />
+        {manageStockMode === 'purchase' && (
+          <InputField
+            loading={loading}
+            label="Purchase price"
+            labelFor="purchase-price"
+            value={purchasePrice.toString()}
+            onChange={(e) => setPurchasePrice(() => parseInt(e.target.value))}
+          />
+        )}
+        <div className="flex flex-row-reverse gap-2 justify-start">
+          <button
+            disabled={loading}
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
+          >
+            Submit
+          </button>
+          <button
+            disabled={loading}
+            type="button"
+            className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200"
+            onClick={() => navigate(-1)}
+          >
+            Cancel
+          </button>
+        </div>
+        {errorMessage && (
+          <p className="text-red-500 text-sm ">{errorMessage}</p>
+        )}
+      </form>
     </PageLayout>
   );
 };
