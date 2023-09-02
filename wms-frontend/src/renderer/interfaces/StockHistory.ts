ac10ba91521944e3c420ae5d5fc4397bfe2067df
Farouq Abdurrahman <farouq03@gmail.com>
2023-09-02 14:09:43 +0200

    updated stock history interface

--- wms-frontend/src/renderer/interfaces/StockHistory.ts
+++ wms-frontend/src/renderer/interfaces/StockHistory.ts
@@ -2,9 +2,10 @@ import { Product } from './Product';
 
 export interface StockHistory {
   id?: string;
+  count: string;
   old_count: string;
-  new_count: string;
   difference: string;
-  updated_at?: string;
+  created_at?: string;
   product: Product;
+  type: string;
 }
