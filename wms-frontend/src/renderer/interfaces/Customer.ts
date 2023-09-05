a26e942de63c36752ee7624737a35ba343d80c61
Farouq Abdurrahman <farouq03@gmail.com>
2023-09-05 19:41:04 +0200

    updated customer interface

--- wms-frontend/src/renderer/interfaces/Customer.ts
+++ wms-frontend/src/renderer/interfaces/Customer.ts
@@ -1,9 +1,10 @@
-import { SpecialPrice } from './SpecialPrice';
-
 export interface Customer {
   id?: string;
   name: string;
   address: string;
   phone_number: string;
-  SpecialPrice: SpecialPrice[];
+  special_price_products: {
+    product: string | null;
+    price: string | null;
+  }[];
 }
