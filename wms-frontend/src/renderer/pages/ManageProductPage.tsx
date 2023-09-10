a42b3960563747fa89c9fc92e763d2f609dfe415
Abdurrahman, Farouq <stfaabdu@stud.h-da.de>
2023-09-10 10:08:48 +0200

    refactored search function in manage stock page

--- wms-frontend/src/renderer/pages/ManageProductPage.tsx
+++ wms-frontend/src/renderer/pages/ManageProductPage.tsx
@@ -1,14 +1,7 @@
-import {
-  collection,
-  doc,
-  getDocs,
-  query,
-  updateDoc,
-} from '@firebase/firestore';
+import { collection, getDocs, query } from '@firebase/firestore';
 import { db } from 'firebase';
-import { FormEvent, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AiOutlineLoading3Quarters } from 'react-icons/ai';
-import { GoPencil } from 'react-icons/go';
 import { IoInformationCircleSharp } from 'react-icons/io5';
 import { useNavigate } from 'react-router-dom';
 import { SingleTableItem } from 'renderer/components/TableComponents/SingleTableItem';
@@ -21,10 +14,7 @@ export const ManageProductPage = () => {
   const [search, setSearch] = useState('');
   const navigate = useNavigate();
   const [products, setProducts] = useState<Product[]>([]);
-  const [editStockToggle, setEditStockToggle] = useState(false);
-  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(false);
-  const [editingIndex, setEditingIndex] = useState<number>(-1);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,7 +31,6 @@ export const ManageProductPage = () => {
         });
 
         setProducts(productData);
-        setFilteredProducts(productData);
         setLoading(false);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -52,42 +41,6 @@ export const ManageProductPage = () => {
       console.log(error);
     });
   }, []);
-
-  useEffect(() => {
-    setFilteredProducts(
-      products.filter((product) =>
-        product.brand
-          .concat(
-            ' ',
-            product.motor_type,
-            ' ',
-            product.part,
-            ' ',
-            product.available_color
-          )
-          .toLowerCase()
-          .includes(search.toLowerCase())
-      )
-    );
-  }, [search, products]);
-
-  const handleUpdateStock = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const product = filteredProducts[editingIndex];
-    if (!product.id) return;
-    const productRef = doc(db, 'product', product.id);
-
-    const updatedProduct = {
-      ...product,
-      count: product.count,
-    };
-
-    updateDoc(productRef, updatedProduct).catch((error) => {
-      console.error('Error updating document: ', error);
-    });
-
-    setEditStockToggle(false);
-  };
 
   return (
     <PageLayout>
@@ -114,69 +67,55 @@ export const ManageProductPage = () => {
                 <th className=" py-3"></th>
               </TableHeader>
               <tbody>
-                {filteredProducts.map((product, index) => (
-                  <tr
-                    key={product.id}
-                    className="border-b hover:shadow-md cursor-pointer hover:underline"
-                    onClick={() =>
-                      product.id && navigate('/manage-product/' + product.id)
-                    }
-                  >
-                    <SingleTableItem>
-                      {product.brand +
-                        ' ' +
-                        product.motor_type +
-                        ' ' +
-                        product.part +
-                        ' ' +
-                        product.available_color}
-                    </SingleTableItem>
-                    <SingleTableItem>
-                      <div
-                        className="flex items-center gap-2"
-                        onClick={(e) => {
-                          e.stopPropagation();
-                          setEditingIndex(index);
-                          if (!editStockToggle) setEditStockToggle(true);
-                        }}
-                      >
-                        {editStockToggle && index === editingIndex ? (
-                          <form onSubmit={(e) => handleUpdateStock(e)}>
-                            <input
-                              autoFocus
-                              onBlur={() => setEditStockToggle(false)}
-                              type="text"
-                              className="bg-gray-100 border border-gray-300 rounded-lg px-2 py-1 w-12 text-center"
-                              defaultValue={product.count}
-                              onChange={(e) => {
-                                product.count = e.target.value;
-                                setProducts([...products]);
-                              }}
-                            />
-                          </form>
-                        ) : (
-                          <>
-                            {product.count}
-                            <GoPencil
-                              size={12}
-                              className="text-gray-500 cursor-pointer"
-                            />
-                          </>
-                        )}
-                      </div>
-                    </SingleTableItem>
-                    <SingleTableItem>{product.sell_price}</SingleTableItem>
-                    <SingleTableItem>
-                      {product.warehouse_position}
-                    </SingleTableItem>
-                    <SingleTableItem>
-                      <IoInformationCircleSharp
-                        size={20}
-                        className="cursor-pointer"
-                      />
-                    </SingleTableItem>
-                  </tr>
-                ))}
+                {products
+                  .filter((product) => {
+                    if (search === '') return product;
+                    else if (
+                      product.brand
+                        .toLowerCase()
+                        .includes(search.toLowerCase()) ||
+                      product.motor_type
+                        .toLowerCase()
+                        .includes(search.toLowerCase()) ||
+                      product.part
+                        .toLowerCase()
+                        .includes(search.toLowerCase()) ||
+                      product.available_color
+                        .toLowerCase()
+                        .includes(search.toLowerCase())
+                    )
+                      return product;
+                  })
+                  .map((product) => (
+                    <tr
+                      key={product.id}
+                      className="border-b hover:shadow-md cursor-pointer hover:underline"
+                      onClick={() =>
+                        product.id && navigate('/manage-product/' + product.id)
+                      }
+                    >
+                      <SingleTableItem>
+                        {product.brand +
+                          ' ' +
+                          product.motor_type +
+                          ' ' +
+                          product.part +
+                          ' ' +
+                          product.available_color}
+                      </SingleTableItem>
+                      <SingleTableItem>{product.count}</SingleTableItem>
+                      <SingleTableItem>{product.sell_price}</SingleTableItem>
+                      <SingleTableItem>
+                        {product.warehouse_position}
+                      </SingleTableItem>
+                      <SingleTableItem>
+                        <IoInformationCircleSharp
+                          size={20}
+                          className="cursor-pointer"
+                        />
+                      </SingleTableItem>
+                    </tr>
+                  ))}
               </tbody>
             </table>
           </div>
