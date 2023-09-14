d7c64bbfa1911f8a129b24daa3a704544976694f
Farouq Abdurrahman <farouq03@gmail.com>
2023-09-14 18:04:29 +0200

    uniformed back button on detail page

--- wms-frontend/src/renderer/pages/ProductDetailPage.tsx
+++ wms-frontend/src/renderer/pages/ProductDetailPage.tsx
@@ -349,7 +349,7 @@ export default function ProductDetailPage() {
                 )}
               </div>
             </div>
-            <div className="w-full flex justify-end">
+            <div className="flex gap-2 w-full justify-between">
               <button
                 type="button"
                 className="px-4 py-2 font-medium text-white bg-gray-600  focus:ring-4 focus:ring-gray-300 rounded-lg text-sm h-[max-content] w-[max-content] flex justify-center gap-2 text-center items-center"
@@ -357,13 +357,11 @@ export default function ProductDetailPage() {
               >
                 Back
               </button>
-            </div>
-            <div className="flex flex-row-reverse gap-2 w-full justify-start">
               {editToggle && (
                 <button
                   disabled={loading}
                   type="submit"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 focus:outline-none"
                 >
                   Submit
                 </button>
--- wms-frontend/src/renderer/pages/SupplierDetailPage.tsx
+++ wms-frontend/src/renderer/pages/SupplierDetailPage.tsx
@@ -243,15 +243,13 @@ export default function SupplierDetailPage() {
                 Back
               </button>
               {editToggle && (
-                <>
-                  <button
-                    disabled={loading}
-                    type="submit"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 focus:outline-none"
-                  >
-                    Submit
-                  </button>
-                </>
+                <button
+                  disabled={loading}
+                  type="submit"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 focus:outline-none"
+                >
+                  Submit
+                </button>
               )}
             </div>
           </form>
