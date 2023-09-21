8108f701cd138eb177a1ad35927013d852b3ef14
bernhard <Bernhard.R.Putranto@stud.h-da.de>
2023-09-21 13:53:46 +0200

    Add date attribute to invoice

--- wms-frontend/src/renderer/interfaces/Invoice.ts
+++ wms-frontend/src/renderer/interfaces/Invoice.ts
@@ -2,6 +2,7 @@ import { Product } from './Product';
 export interface Invoice {
   id?: string;
   customer_id?: string;
+  date?: string;
   items?: Product[];
   payment_method?: string;
   total_price?: string;
