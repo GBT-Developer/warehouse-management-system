39c4a98951c891403333f01d95e4484254a639fa
stvitanh <vinsky.s.tanhansen@stud.h-da.de>
2023-10-01 02:55:38 +0200

    add more info to void list

--- wms-frontend/src/renderer/pages/VoidListPage.tsx
+++ wms-frontend/src/renderer/pages/VoidListPage.tsx
@@ -150,7 +150,8 @@ export default function VoidListPage() {
                                   className="py-[0.75rem]"
                                 >
                                   <div>
-                                    {product.brand}: {product.count}x
+                                    {product.brand} {product.part}{' '}
+                                    {product.available_color}: {product.count}x
                                   </div>
                                 </div>
                               ))}
