dedb3006bf4b60f444871bfda0580f873a600322
Farouq Abdurrahman <farouq03@gmail.com>
2023-10-01 14:47:49 +0200

    removed forget pass

--- wms-frontend/src/renderer/pages/LoginPage.tsx
+++ wms-frontend/src/renderer/pages/LoginPage.tsx
@@ -97,12 +97,6 @@ export const AuthPage = () => {
               />
             </label>
           </div>
-          <a
-            className="text-sm text-blue-700 hover:underline ml-auto"
-            href="https://google.com"
-          >
-            Masa lupa password sih?
-          </a>
           <button
             type="submit"
             className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center flex justify-center"
