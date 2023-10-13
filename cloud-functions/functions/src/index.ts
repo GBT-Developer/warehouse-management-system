6ce97862e75b3dc444b89f837d35082dd7e8be9f
Abdurrahman, Farouq <stfaabdu@stud.h-da.de>
2023-10-14 01:14:42 +0200

    commented out unnecessary seed

--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -18,21 +18,21 @@ export * from "./getUsers";
 
 if (process.env.NODE_ENV === "development") {
   const NUM_USERS = 10;
-  const NUM_SUPPLIERS = 15;
-  const NUM_PRODUCTS = 50;
-  const NUM_BROKEN_PRODUCTS = 10;
-  const NUM_CUSTOMER = 15;
-  const NUM_TRANSACTIONS = 17;
+  // const NUM_SUPPLIERS = 15;
+  // const NUM_PRODUCTS = 50;
+  // const NUM_BROKEN_PRODUCTS = 10;
+  // const NUM_CUSTOMER = 15;
+  // const NUM_TRANSACTIONS = 17;
 
   import("./seeding").then(async (module) => {
     await module.seedUser(NUM_USERS); // Create 10 users and one 'owner' user
     await module.seedCompanyInfo(); // Create company info
-    const suppliers = await module.seedSupplier(NUM_SUPPLIERS); // Create 10 suppliers
-    if (suppliers.size > 0) {
-      await module.seedProduct(NUM_PRODUCTS, suppliers); // Create 100 products
-    }
-    await module.seedBrokenProduct(NUM_BROKEN_PRODUCTS, suppliers); // Create 10 broken products
-    await module.seedCustomer(NUM_CUSTOMER); // Create 15 customers
-    await module.seedTransaction(NUM_TRANSACTIONS); // Create 10 transactions
+    // const suppliers = await module.seedSupplier(NUM_SUPPLIERS); // Create 10 suppliers
+    // if (suppliers.size > 0) {
+    //   await module.seedProduct(NUM_PRODUCTS, suppliers); // Create 100 products
+    // }
+    // await module.seedBrokenProduct(NUM_BROKEN_PRODUCTS, suppliers); // Create 10 broken products
+    // await module.seedCustomer(NUM_CUSTOMER); // Create 15 customers
+    // await module.seedTransaction(NUM_TRANSACTIONS); // Create 10 transactions
   });
 }
