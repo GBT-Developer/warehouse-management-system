75f29c7c4684a8cd219301ad5a6e7167f06dca3d
Vinsky Strauss TanHanSen <vinsky.s.tanhansen@stud.h-da.de>
2023-10-13 19:15:47 +0200

    change login in change password

--- wms-frontend/src/renderer/pages/ChangePasswordPage.tsx
+++ wms-frontend/src/renderer/pages/ChangePasswordPage.tsx
@@ -96,7 +96,7 @@ export const ChangePasswordPage = () => {
                 }}
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
               >
-                Submit
+                Konfirmasi
               </button>
             </form>
           </div>
